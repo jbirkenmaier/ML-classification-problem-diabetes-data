03e21eeee5b193eef22de1d6cf9980fa6ace40f8
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-06-22 11:47:07 +0200

    update

--- function_library.py
+++ function_library.py
@@ -1,6 +1,6 @@
 def read_data(name_of_file):
     with open('%s'%name_of_file) as datafile:
-        attributes = [datafile.readline().split()]
+        attributes = datafile.readline().split()
         print(attributes)
         
 
