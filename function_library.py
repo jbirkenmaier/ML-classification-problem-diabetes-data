a432eea84af6f3c22a70707677666d458d536148
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-06-22 12:02:55 +0200

    update

--- function_library.py
+++ function_library.py
@@ -1,3 +1,5 @@
+
+#function for reading the data 
 def read_data(name_of_file):
     data=[]
     with open('%s'%name_of_file) as datafile:
@@ -5,8 +7,10 @@ def read_data(name_of_file):
         for line in datafile:
             data.append(line.replace('\n', '').split(','))
         return data
-
-        
-        
+    
+#now I want to slice the data into a "Training-set" and a "Testing-set"
+#The "Training-set" will be used for training the algorithm. The "Testing-set" will be used for testing and evaluating.
+#I will assign 75% of the dataset for training purposes and 25% for testing.
+    
 
     
