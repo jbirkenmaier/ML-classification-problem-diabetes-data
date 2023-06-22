b28db0664f87963eb240259a5d2ca847400a9fd1
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-06-22 11:46:22 +0200

    update

---
+++ function_library.py
@@ -0,0 +1,7 @@
+def read_data(name_of_file):
+    with open('%s'%name_of_file) as datafile:
+        attributes = [datafile.readline().split()]
+        print(attributes)
+        
+
+    
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import function_library as fl
+
+
+fl.read_data('diabetes_data_upload.csv')
