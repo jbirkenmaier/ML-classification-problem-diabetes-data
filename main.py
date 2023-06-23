7895f61027888c4460ea90200aee771d93eb6186
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-06-23 11:56:56 +0200

    update

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import function_library as fl
 
 
 data = fl.read_data('diabetes_data_upload.csv')[0]
+(training_data, test_data)= (fl.split_training_testing(data))
 
-fl.split_training_testing(data)
+print(len(training_data))
+print(len(test_data))
+print(len(data))
