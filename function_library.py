d136b4ee5a1c30da5c1f7dc50ac049dc795685e9
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-06-26 10:41:37 +0200

    update

--- function_library.py
+++ function_library.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 #function for reading the data 
 def read_data(name_of_file):
@@ -33,6 +34,41 @@ def split_training_testing(data):
 As the provided data is mostly binary, with exception of the patient age, feature scaling might not be necessary.
 I will see if that is true later in the progress of the project. For now I will leave out feature scaling.
 '''
+
+def model_function(x,w,b):
+    return np.dot(x,w)+b
+
+def sig(value):
+    return 1/(1+np.exp(-value))
+
+def loss(model_estimate,y):
+    d = sig(model_estimate)
+    if y == 1:
+        return -np.log(d)
+    if y == 0:
+        return -np.log(1-f)
+
+def cost(x,w,b,y):
+    m=len(x)
+    J=0
+    for i in range(m):
+        J+=loss(w,b,x[i],y[i])
+    return J/m
+
+def derivative_of_cost(w,b,x,y,lamb):
+    m = len(x)
+    n = len(w)
+
+    dJ_dw = np.zeros(n)
+    dJ_db = 0
+    l=0
+    x=np.array(x)
+    x_list = x
+    loss_list = np.array([loss(w,b,x,y) for (x,y) in zip(x,y)])
+    dJ_dw = np.dot(loss_list, x_list) + lamb /m*w
+    dJ_db = np.sum(loss_list)
+
+    return dJ_dw/m, dJ/db/m
 
 #function for gradient descent
 def gradient_descent():
